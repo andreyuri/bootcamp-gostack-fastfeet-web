11665f55875b632ea9ab83df1f00dbb9c437ce09
André Yuri <andre_yuri@live.com>
2020-03-31 19:53:06 -0300

    Estilização da autenticação

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import logo from '~/assets/images/logo.png';
 
 export default function SignIn() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="FastFeet" />
+
+      <form>
+        <label htmlFor="email">Seu e-mail</label>
+        <input id="email" type="email" placeholder="exemplo@email.com" />
+        <label htmlFor="password">Sua senha</label>
+        <input id="password" type="password" placeholder="*********" />
+
+        <button type="submit">Entrar no sistema</button>
+      </form>
+    </>
+  );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -5,4 +5,47 @@ import colors from '~/styles/colors';
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-90deg, ${colors.primary}, ${colors.second});
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 350px;
+  text-align: center;
+  background: #fff;
+  padding: 40px 30px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    label {
+      text-align: left;
+      font-weight: bold;
+      text-transform: uppercase;
+      font-size: 12px;
+      margin-top: 10px;
+    }
+
+    input {
+      background: #fff;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      margin: 10px 0 10px;
+      border: 1px solid #d3d3d3;
+    }
+
+    button {
+      color: #fff;
+      background: ${colors.primary};
+      padding: 12px 22px;
+      border-radius: 4px;
+      border: 0;
+      margin-top: 10px;
+    }
+  }
 `;
