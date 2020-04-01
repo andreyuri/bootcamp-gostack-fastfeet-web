f3cccea0c6618894d8aebe91a420ad22201a9c3c
André Yuri <andre_yuri@live.com>
2020-04-01 13:41:09 -0300

    Requisições autenticadas

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -22,6 +22,7 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
     yield put(signInSuccess(token, user));
     history.push('/delivery');
   } catch (err) {
@@ -30,4 +31,17 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);
