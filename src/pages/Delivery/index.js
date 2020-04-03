b0414d29aefd3bda2097bf6b580ac25520eeb499
André Yuri <andre_yuri@live.com>
2020-04-02 21:33:42 -0300

    tabela de encomendas

--- src/pages/Delivery/index.js
+++ src/pages/Delivery/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import { MdAdd } from 'react-icons/md';
+import React, { useState, useEffect } from 'react';
+import { MdAdd, MdMoreHoriz, MdFiberManualRecord } from 'react-icons/md';
 
-import { Container, HeaderBody, AddButton } from './styles';
+import api from '~/services/api';
+
+import {
+  Container,
+  HeaderBody,
+  AddButton,
+  Table,
+  Deliveryman,
+  Status,
+  ActionsButton,
+} from './styles';
 
 export default function Delivery() {
+  const [delivery, setDelivery] = useState([]);
+
+  useEffect(() => {
+    async function loadDelivery() {
+      const response = await api.get('/deliveries');
+
+      const data = response.data.map((d) => ({
+        ...d,
+        firstLetters:
+          d.deliveryman.name.split(' ')[0].substring(0, 1) +
+          d.deliveryman.name.split(' ')[1].substring(0, 1),
+      }));
+
+      setDelivery(data);
+    }
+
+    loadDelivery();
+  }, []);
+
   return (
     <Container>
       <header>
@@ -16,6 +45,49 @@ export default function Delivery() {
           </AddButton>
         </HeaderBody>
       </header>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Destinatário</th>
+            <th>Entregador</th>
+            <th>Cidade</th>
+            <th>Produto</th>
+            <th>Status</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {delivery.map((item) => (
+            <tr key={String(item.id)}>
+              <td>#{item.id}</td>
+              <td>{item.recipient.name}</td>
+              <Deliveryman color={item.color}>
+                <span>{item.firstLetters}</span>
+                {item.deliveryman.name}
+              </Deliveryman>
+              <td>{item.recipient.city}</td>
+              <td>{item.product}</td>
+              <td>
+                <Status
+                  entregue={item.status === 'Entregue'}
+                  cancelada={item.status === 'Cancelado'}
+                  retirada={item.status === 'Retirada'}
+                >
+                  <MdFiberManualRecord size={10} />
+                  {item.status}
+                </Status>
+              </td>
+              <td>
+                <ActionsButton>
+                  <MdMoreHoriz size={24} color="black" />
+                </ActionsButton>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Container>
   );
 }
--- src/pages/Delivery/styles.js
+++ src/pages/Delivery/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { darken, lighten } from 'polished';
 
 import colors from '~/styles/colors';
@@ -61,4 +61,101 @@ export const AddButton = styled.button`
   &:hover {
     background: ${darken(0.1, colors.primary)};
   }
+`;
+
+export const Table = styled.table.attrs({
+  rules: 'none',
+  border: 0,
+})`
+  width: 100%;
+  text-align: left;
+  border-spacing: 0 20px;
+
+  thead {
+    th {
+      padding: 5px 20px;
+    }
+    th:last-child {
+      text-align: right;
+    }
+  }
+
+  tbody {
+    background: #fff;
+
+    td {
+      padding: 10px 20px;
+      height: 38px;
+      border: 0;
+
+      &:last-child {
+        text-align: right;
+        opacity: 0.9;
+      }
+    }
+  }
+`;
+
+export const Deliveryman = styled.td`
+  justify-content: center;
+  align-items: center;
+  text-transform: capitalize;
+
+  span {
+    margin-right: 8px;
+    font-weight: bold;
+    padding: 7px;
+    border-radius: 50px;
+    color: #fff;
+    text-transform: uppercase;
+
+    ${(props) =>
+      props.color &&
+      css`
+        background: ${props.color};
+      `}
+  }
+`;
+
+export const Status = styled.span`
+  font-weight: bold;
+  text-transform: uppercase;
+  padding: 5px 10px;
+  border-radius: 10px;
+  font-size: 12px;
+
+  svg {
+    margin-right: 4px;
+  }
+
+  background: ${lighten(0.5, '#a0a016')};
+  color: #a0a016;
+
+  ${(props) =>
+    props.entregue &&
+    css`
+      background: ${lighten(0.5, '#426942')};
+      color: #426942;
+    `}
+
+  ${(props) =>
+    props.cancelada &&
+    css`
+      background: ${lighten(0.2, '#de5c5c')};
+      color: #de5c5c;
+    `}
+
+  ${(props) =>
+    props.retirada &&
+    css`
+      background: ${lighten(0.35, '#2727dc')};
+      color: #2727dc;
+    `}
+`;
+
+export const ActionsButton = styled.button.attrs({
+  type: 'button',
+})`
+  background: none;
+  border: 0;
 `;
