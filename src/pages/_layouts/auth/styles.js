f0b6f0fd4cf8b3bd4ecafec10090d6deccab6776
André Yuri <andre_yuri@live.com>
2020-04-18 13:13:52 -0300

    altura da tela

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import colors from '~/styles/colors';
 
 export const Wrapper = styled.div`
-  height: 100%;
+  height: 100vh;
   background: linear-gradient(-90deg, ${colors.primary}, ${colors.second});
   display: flex;
   justify-content: center;
